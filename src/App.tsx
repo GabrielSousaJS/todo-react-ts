464804f37c608386c30d441d27538e051000de95
Gabriel Santos <gabriels.santosx@gmail.com>
2026-04-27 22:44:32 -0300

    Componente de icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import Text from './components/text';
+import TrashIcon from './assets/icons/transh.svg?react';
+import SpinnerIcon from './assets/icons/spinner.svg?react';
+import Icon from './components/icons';
 
 export default function App() {
   return (
-    <Text variant="body-sm-bold" className="text-pink-base">
-      Olá mundo
-    </Text>
+    <div>
+      <div className="grid gap 3">
+        <Text variant="body-sm-bold" className="text-pink-base">
+          Olá mundo
+        </Text>
+      </div>
+      <div>
+        <TrashIcon className="fill-pink-base" />
+        <Icon svg={SpinnerIcon} animate />
+      </div>
+    </div>
   );
 }
